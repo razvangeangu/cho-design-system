363b97bf97af247ccd01e415599228eee75808d1
Răzvan-Gabriel Geangu <razvan.geangu@gmail.com>
2020-04-25 04:26:21 +0100

    changed index to examples from router

---
+++ packages/cho-components/src/components/inputs/cho-button/examples/ChoButtonPrimary.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { h } from '@stencil/core';
+
+export const ChoButtonPrimary = () => (
+  <cho-button>
+    <span>Primary</span>
+  </cho-button>
+);
---
+++ packages/cho-components/src/components/inputs/cho-button/examples/index.ts
@@ -0,0 +1,2 @@
+export * from './ChoButtonPrimary';
+export * from './ChoButtonSecondary';
