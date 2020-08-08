e7f3611855a232b174d62f0db3538011e5af91c9
Răzvan-Gabriel Geangu <razvan.geangu@gmail.com>
2020-08-08 19:12:08 +0100

    chore: add angular proxies

--- packages/cho-components-angular/src/cho-components-module.ts
+++ packages/cho-components-angular/src/cho-components-module.ts
@@ -3,37 +3,76 @@ import { defineCustomElements } from '@cho/components/loader';
 import { BooleanValueAccessor } from './directives/boolean-value-accessor';
 import { NumericValueAccessor } from './directives/number-value-accessor';
 import {
+  ChoAppBar,
+  ChoBackdrop,
+  ChoBadge,
+  ChoBreadcrumbs,
   ChoButton,
   ChoCheckbox,
+  ChoChip,
+  ChoDatePicker,
+  ChoDialog,
   ChoDivider,
+  ChoDrawer,
+  ChoExpansionPanel,
+  ChoExpansionPanelItem,
+  ChoIcon,
+  ChoLink,
   ChoMenu,
   ChoMenuItem,
   ChoMenuItemGroup,
+  ChoNotification,
+  ChoProgress,
   ChoRadio,
   ChoSelect,
   ChoSlider,
   ChoSwitch,
+  ChoTabItem,
+  ChoTabItemContent,
+  ChoTabs,
   ChoTextField,
+  ChoTimePicker,
+  ChoTooltip,
 } from './directives/proxies';
 import { RadioValueAccessor } from './directives/radio-value-accessor';
 import { SelectValueAccessor } from './directives/select-value-accessor';
 import { TextValueAccessor } from './directives/text-value-accessor';
+import { ValueAccessor } from './directives/value-accessor';
 
 defineCustomElements(window);
 
 const DECLARATIONS = [
   // proxies
+  ChoAppBar,
+  ChoBackdrop,
+  ChoBadge,
+  ChoBreadcrumbs,
   ChoButton,
   ChoCheckbox,
+  ChoChip,
+  ChoDatePicker,
+  ChoDialog,
   ChoDivider,
+  ChoDrawer,
+  ChoExpansionPanel,
+  ChoExpansionPanelItem,
+  ChoIcon,
+  ChoLink,
   ChoMenu,
   ChoMenuItem,
   ChoMenuItemGroup,
+  ChoNotification,
+  ChoProgress,
   ChoRadio,
   ChoSelect,
   ChoSlider,
   ChoSwitch,
+  ChoTabItem,
+  ChoTabItemContent,
+  ChoTabs,
   ChoTextField,
+  ChoTimePicker,
+  ChoTooltip,
 
   // Value Accessors
   BooleanValueAccessor,
@@ -41,6 +80,7 @@ const DECLARATIONS = [
   RadioValueAccessor,
   SelectValueAccessor,
   TextValueAccessor,
+  ValueAccessor,
 ];
 
 @NgModule({
