173ca309da6c698eeb1e7594094b08c6157962f4
Răzvan-Gabriel Geangu <razvan.geangu@gmail.com>
2020-08-23 10:57:26 +0100

    refactor: remove site

--- packages/cho-site/README.md
+++
@@ -1,3 +0,0 @@
-# @cho/site
-
-This is a presentational site of the @cho/compponents as part of a design system.
--- packages/cho-site/src/utils/sentence-case.ts
+++
@@ -1,5 +0,0 @@
-export const sentenceCase = (text: string) =>
-  text
-    .replace(/-([a-z])/g, (_, word) => word.toUpperCase())
-    .replace(/([A-Z])/g, match => ` ${match}`)
-    .replace(/^./, match => match.toUpperCase());
